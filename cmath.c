8d88858751408fe36566d9bf3f22e0d9ebfe4071
apage-pi <72623953+apage-pi@users.noreply.github.com>
2020-10-13 09:37:48 -0500

    Add files via upload

---
+++ cmath.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main (void)
+{
+ float a;
+ float b;
+ float c;
+ a = 2.5;
+ b = 7.5;
+ c = a + b;
+ c = a + b;
+ c = a + b;
+ printf ("The sum of adding %f and %f is %f\n", a, b, c);
+}
